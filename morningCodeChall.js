3847aa6ed3830c9070981fd5b2f5146b272a00b1
Scott Nelson <scottninlv@yahoo.com>
2023-07-20 08:38:26 -0700

    Monring Ex.

---
+++ morningCodeChall.js
@@ -0,0 +1,13 @@
+// const hasUniqueChars = (chars) => {
+//  let uniq = new set([])
+//  let splitChars = chars.split('')
+//  for (let i = 0; i < splitChars.length; i++) {
+//     if(uniq.idexOf(splitChars[i]) === -1) {
+//         uniq.push(arr[i])
+//     } 
+
+// }
+// return uniq
+// }
+
+
